49c1a7be6ec7d85a8944a2a4436a1142daf7d09f
evanbones <bownessevan@gmail.com>
2025-11-29 16:01:47 -0800

    Fix launch path

--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
 from setuptools import setup
-from glob import glob
 import os
 
 package_name = 'tune_gui'
@@ -12,7 +11,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
+        (os.path.join('share', package_name, 'launch'), ['launch/tune_gui_launch.py']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
